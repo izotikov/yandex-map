2ba8cec73a001868bd059823b3ad8e0a68c83e62
Izotikov <izotikov.igor1@yandex.ru>
2022-08-25 17:29:41 +0300

    Итоговый проект

---
+++ src/functions/findClosestTo.js
@@ -0,0 +1,16 @@
+
+// Код нахождения ближайшей точки МКАД.
+
+export function findClosestTo(mkadCords, markerCords) {
+  let tempCordsArr = [];
+  let digitsArr =[];
+  for (let i=0; i < mkadCords.length; i++) {
+    let d = Math.sqrt(Math.pow(markerCords[0] - mkadCords[i][0], 2) + Math.pow(markerCords[1] - mkadCords[i][1], 2));
+    let newobj = {id: i, digit: d};
+    tempCordsArr.push(newobj);
+    digitsArr.push(d);
+  }
+  let closest = tempCordsArr.find(elem => Math.min(...digitsArr) === elem.digit);
+  let neededMkadCords = mkadCords[closest.id];
+  return neededMkadCords;
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import YmapPlugin from 'vue-yandex-maps';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+const settings = {
+  apiKey: '186f0adf-8b02-43df-a8ae-8c25e6a58865',
+  lang: 'ru_RU',
+  enterprise: false,
+  version: '2.1',
+};
+
+app.use(YmapPlugin, settings);
+app.mount("#app");
